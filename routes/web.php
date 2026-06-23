75f386b35b774c1af1d81f2d29da4238ec7687c9
Sianturisamuel <sianturisamuel23@gmail.com>
2026-06-23 15:01:09 +0700

    Add Supplier CRUD

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,8 @@ Route::get('/', function () {
 
 Route::resource('obat', ObatController::class);
 
+Route::resource('supplier', SupplierController::class);
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
